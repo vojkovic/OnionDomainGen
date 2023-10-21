8e42c060e58efcd3d2fdb9d76a3c7a0064bd22df
Brock Vojkovic <brockv@tuta.io>
2023-10-21 13:03:38 +1100

    Include all in main definition

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import base64
 # Implementation from the Tor v3 specification.
 # https://github.com/torproject/torspec/blob/main/rend-spec-v3.txt#L2259C1-L2272C81
 
-with open('hs_ed25519_public_key', 'rb') as key_file:
-    key_file.seek(32)
-    key_bytes = key_file.read()
-
-if len(key_bytes) != 32:
-    print(len(key_bytes))
-    raise ValueError("Invalid Ed25519 public key length")
-
 def main():
+    with open('hs_ed25519_public_key', 'rb') as key_file:
+        key_file.seek(32)
+        key_bytes = key_file.read()
+    
+    if len(key_bytes) != 32:
+        print(len(key_bytes))
+        raise ValueError("Invalid Ed25519 public key length")
+
     VERSION = b'\x03'
     CHECKSUM_CONSTANT = b".onion checksum"
 
